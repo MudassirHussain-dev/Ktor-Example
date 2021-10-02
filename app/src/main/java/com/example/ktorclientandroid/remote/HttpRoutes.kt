dd8170037a7113f6204cdf08b9fda5a64929cea3
AAM POWER DEVELOPER <AAM POWER DEVELOPER@DESKTOP-FLAPIQK>
2021-10-02 16:42:28 +0500

    Ktor recyclerivew example

---
+++ app/src/main/java/com/example/ktorclientandroid/remote/HttpRoutes.kt
@@ -0,0 +1,7 @@
+package com.example.ktorclientandroid.remote
+
+object HttpRoutes {
+
+    private const val BASE_URL = "https://jsonplaceholder.typicode.com"
+    const val POSTS = "$BASE_URL/posts"
+}
---
+++ app/src/main/java/com/example/ktorclientandroid/remote/PostsService.kt
@@ -0,0 +1,30 @@
+package com.example.ktorclientandroid.remote
+
+import com.example.ktorclientandroid.remote.dto.PostRequest
+import com.example.ktorclientandroid.remote.dto.PostResponse
+import io.ktor.client.*
+import io.ktor.client.engine.android.*
+import io.ktor.client.features.json.*
+import io.ktor.client.features.json.serializer.*
+import io.ktor.client.features.logging.*
+
+interface PostsService {
+    suspend fun getPosts(): List<PostResponse>
+
+    suspend fun createPost(postRequest: PostRequest): PostResponse?
+
+    companion object {
+        fun create(): PostsService {
+            return PostsServiceImpl(
+                client = HttpClient(Android) {
+                    install(Logging) {
+                        level = LogLevel.ALL
+                    }
+                    install(JsonFeature) {
+                        serializer = KotlinxSerializer()
+                    }
+                }
+            )
+        }
+    }
+}
